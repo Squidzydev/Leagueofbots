777538cc43d08af55f2008dfad489dcc6a6db290
Squidzydev <53304905+Squidzydev@users.noreply.github.com>
2020-05-11 00:10:24 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -236,7 +236,7 @@ async def help(ctx):
     message = await ctx.send(embed = embed)
     await asyncio.sleep(10)
     await message.delete()
-@Bot.command()
+@Bot.command(aliases=['руны'])
 async def runes(ctx, *, args):
     await ctx.message.delete()
     name_of_hero = ''
@@ -280,7 +280,7 @@ async def runes(ctx, *, args):
 
 
 
-@Bot.command()
+@Bot.command(aliases=['ранк', 'ранг'])
 async def rank(ctx, *,args):
     name = ''
     await ctx.message.delete()
